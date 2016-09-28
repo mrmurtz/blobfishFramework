dd301e694235c7a16df3e972aeb69b1d1921e066
Tam <tamborine@Tams-MacBook-Air.local>
2016-09-28 10:27:12 +0100

    first commit #37

---
+++ test.js
@@ -0,0 +1,13 @@
+
+arrayofNotes[index]
+
+
+notes ["fsdfsd", "fsdfsd"]
+
+function HtmlListInjection() {
+  app.listNotes.forEach(function(note){
+  var index = IndexOf()
+   return <li id=index>note </li>
+  })
+
+}
